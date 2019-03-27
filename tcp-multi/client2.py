3c77761ccd25d2bb6c35f0e1d215534bbbb17ccb
ryoookaze <32873309+ryoookaze@users.noreply.github.com>
2019-03-27 04:31:32 -0700

    Add files via upload

--- tcp-multi/client2.py
+++ tcp-multi/client2.py
@@ -10,36 +10,57 @@ client_path = "./clientfolder2/"
 image_name = ["lambo1.jpg", "lambo2.jpg"]
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+def changefolder():
+    os.chdir(client_path)
+
 def Main():
     s = socket.socket()
     s.connect((TCP_IP, TCP_PORT))
 
-    filename = raw_input("Filename? -> ")
-    if filename != 'q':
-        s.send(filename)
-        data = s.recv(4096)
-        if data[:6] == 'EXISTS':
-            filesize = long(data[6:])
-            message = raw_input("File exists, " + str(filesize) +"Bytes, download? (Y/N)? -> ")
-            if message == 'Y':
-                s.send("OK")
-                f = open('new_'+filename, 'wb')
-                data = s.recv(4096)
-                totalRecv = len(data)
-                f.write(data)
-                while totalRecv < filesize:
-                    data = s.recv(4096)
-                    totalRecv += len(data)
-                    f.write(data)
-                    print "{0:.2f}".format((totalRecv/float(filesize))*100)+ "% Done"
-                print "Download Complete!"
-                f.close()
-        else:
-            print "File Does Not Exist!"
-
-    s.close()
+    print "1. List image 2. Request Image 3. Send Image"
+    input_req = raw_input(">")
+    if(input_req == '1'):
+        msg = sock.recv(buffsize)
+        print msg
     
-
+    elif(input_req == '2'):
+        if not os.path.exists(client_path):
+            os.mkdir(client_path)
+            changefolder()
+            filename = raw_input("Filename? -> ")
+            if filename != 'q':
+                s.send(filename)
+                data = s.recv(buffsize)
+                if data[:6] == 'EXISTS':
+                    filesize = long(data[6:])
+                    message = raw_input("File exists, " + str(filesize)+"Bytes, download? (Y/N)? -> ")
+                    if message == 'Y':
+                        s.send("OK")
+                        f = open('new_'+filename, 'wb')
+                        data = s.recv(buffsize)
+                        totalRecv = len(data)
+                        f.write(data)
+                        while totalRecv < filesize:
+                            data = s.recv(buffsize)
+                            totalRecv += len(data)
+                            f.write(data)
+                            print "{0:.2f}".format((totalRecv/float(filesize))*100)+"% Done"
+                        print "Download Complete!"
+                        f.close()
+                else:
+                    print "File Does Not Exist!"
+            s.close()
+    elif(input_req=='3'):
+        name = raw_input("Masukkan nama file -> ")
+        sock.send("START {}" . format(name))
+        size = os.stat(nama).st_size
+        fp = open(name,'rb')
+        i = fp.read()
+        sent = 0
+        for x in i:
+            sock.send(x)
+            sent += 1
+        fp.close()
+    
 if __name__ == '__main__':
     Main()
-
