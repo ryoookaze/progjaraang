c5bb99ab51d584a4d26403b9fa68e19c9bfbf03d
ryoookaze <32873309+ryoookaze@users.noreply.github.com>
2019-05-23 23:14:06 +0700

    Add files via upload

---
+++ Progjar Tugas 3/client.py
@@ -0,0 +1,43 @@
+import socket
+import os
+
+client_path = "./client/"
+#completePath = os.path.join(client_path, f)
+
+def Main():
+    host = '127.0.0.1'
+    port = 5000
+
+    s = socket.socket()
+    s.connect((host, port))
+
+    filename = raw_input("Filename? -> ")
+    if filename != 'q':
+        s.send(filename)
+        data = s.recv(1024)
+        if data[:6] == 'EXISTS':
+            filesize = long(data[6:])
+            message = raw_input("File exists, " + str(filesize) +"Bytes, download? (Y/N)? -> ")
+            if message == 'Y':
+                client_path = "./client/"
+                completePath = os.path.join(client_path, filename)
+                s.send("OK")
+                f = open(completePath, 'wb')
+                data = s.recv(1024)
+                totalRecv = len(data)
+                f.write(data)
+                while totalRecv < filesize:
+                    data = s.recv(1024)
+                    totalRecv += len(data)
+                    f.write(data)
+                    print "{0:.2f}".format((totalRecv/float(filesize))*100)+ "% Done"
+                print "Download Complete!"
+                f.close()
+        else:
+            print "File Does Not Exist!"
+
+    s.close()
+    
+
+if __name__ == '__main__':
+    Main()
